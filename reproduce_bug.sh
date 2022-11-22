3fe484a7e8cf8c807656fc2a3dc1598308eab112
agusnt <agusnavarro11@gmail.com>
2022-11-22 11:39:16 +0100

    Remove script

--- reproduce_bug.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#rm -r bin
-#make clean
-./config.sh berti.json
-make
-#./bin/no --warmup_instructions 50000000 --simulation_instructions 200000000\
-./bin/no --warmup_instructions 0 --simulation_instructions 10000000\
-    /home/ant_uz/HW_Prefetch/Trazas/spec2k17/602.gcc_s-1850B.champsimtrace.xz
